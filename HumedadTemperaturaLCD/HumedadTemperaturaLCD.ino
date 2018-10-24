f1dc2f344a1265521f3a61cab38a2333a49d5647
PaulEsparza <esparza.gnp10@gmail.com>
2018-10-24 17:51:27 -0500

    Modificacion en escritura a LCD

--- HumedadTemperaturaLCD/HumedadTemperaturaLCD.ino
+++ HumedadTemperaturaLCD/HumedadTemperaturaLCD.ino
@@ -1,17 +1,17 @@
 /*
     Definimos las librerias que se van a utilizar, una para que
     la placa de arduino obtenga los valores de humedad y temperatura
-    y otra para el funcionamiento de la pantalla LCD respectivamente
+    otra para el funcionamiento de la pantalla LCD y la libreria Time
+    que nos permitira obtener una fecha y hora en tiempo real respectivamente
 */
 #include "DHT.h"
-//#include <LiquidCrystal.h>
-#include <LiquidCrystal_I2C.h> // usamos la version 1.2.1 compatible con nuestra lcd 16x2
+#include <LiquidCrystal.h>
+#include <TimeLib.h> 
 
 /*
    Definimos las variables y constantes junto con los pin utilizados en la placa arduino
 */
 
-/*
 // PIN's utilizados por la pantalla LCD
 #define RS 12
 #define E 11
@@ -19,23 +19,15 @@
 #define D5 9
 #define D6 8
 #define D7 7
-*/
+#define pushSiguiente 5
+#define pushAnterior 4
 
-//LiquidCrystal lcd(RS, E, D4, D5, D6, D7); // Inicializamos la pantalla LCD
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //Inicializamos la pantalla LCD
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7); // Inicializamos la pantalla LCD
 
 // PIN's utilizados por el sensor de humedad y temperatura
-#define DHTPIN 2  // PIN digital donde se conecta el sensor
+#define DHTPIN A1  // PIN digital donde se conecta el sensor
 #define DHTTYPE DHT11 // Definimos el tipo de sensor de humedad y temperatura utilizado, en este caso sera DHT11
 DHT dht(DHTPIN, DHTTYPE); // Inicializamos el sensor DHT11
-
-//Variables usadas para guardar lo que imprimiremos en nuestra lcd
-String mensajeEstadoTiempo;     // donde pondremos las 
-String mensajeUsuario;
-
-//Variables utilizadas para lectura de datos del sensor dht11
-float humedad;
-float temperatura;
 
 // Variables utilizadas para lectura de datos de la fotorresistencia
 const long A = 1000;     //Resistencia en oscuridad en KΩ
@@ -46,11 +38,21 @@ const int LDRPIN = A0;   //Pin analogico de entrada del LDR
 int valorActual; // Variable donde se almacena el valor de luminosidad
 int valorLuminosidad; // Variable que nos servira para guardar el valor calculado de luminosidad que se tenga actualmente
 
+int imprimir=0;
+String Mensaje="";
+int indice = 0;
+
 void setup() {
-  lcd.begin(16, 4); // Definimos el tipo de pantalla LCD utilizada
+  pinMode(10,OUTPUT);
+digitalWrite(10,HIGH);
+pinMode(pushSiguiente, INPUT);
+pinMode(pushAnterior, INPUT);
+  lcd.begin(20, 4); // Definimos el tipo de pantalla LCD utilizada
   dht.begin(); // Comenzamos el sensor DHT
   Serial.begin(9600); // Inicializamos la comunicacion serie
-  lcd.write("Bienvenido!!! :)");
+  //Anotamos la hora y la fecha desde la que nuestro programa empezará a contar.
+  //Formato: hora, minutos, segundos, días, mes, año
+  setTime(1,00,00,20,10,2018);
 }
 
 /*
@@ -59,50 +61,13 @@ void setup() {
    luminosidad: Calcula el valor en Luxes de luminosidad actual y los muestra en la pantalla LCD
 */
 void loop() {
-  enviarDatos();      //Datos que se envían a la interfaz de software
-  recibirDatos();     //Datos que se reciben por medio de la interfaz de software
-  //humedadTemperatura();
-  //luminosidad();
-}
-
-void recibirDatos(){
-  mensajeEstadoTiempo = Serial.readStringUntil('!');
-    mensajeUsuario = Serial.readStringUntil('!');
-    
-    displayLCD();
-    
-  if(Serial.available() > 0){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("cargando...");
-    
-    
-    
+  humedadTemperatura();
+  luminosidad();
+  fechaHora();
+  LCD();
+  while(analogRead(pushSiguiente) == HIGH){
+    Serial.println("Siguiente");
   }
-}
-
-/**
- * Metodo que se encargar de enviar los datos del sensor DHT11 (humedad y temperatura) y
- * el valor de la fotoresistencia (luminosidad) por medio del puerto serie, para que 
- * la computadora los pueda leer
- */
-void enviarDatos(){
-  delay(2000);
-  humedad = dht.readHumidity();
-  temperatura = dht.readTemperature();
-  valorActual = analogRead(LDRPIN); //valor de la fotoresistencia
-  valorLuminosidad = ((long)valorActual * A * 10) / ((long)B * RC * (1024 - valorActual)); // Formula utilizada porque el LDR esta entre A0 y Vcc
-  Serial.println(humedad);
-  Serial.println(temperatura);  
-  Serial.println(valorLuminosidad/10);
-}
-
-void displayLCD(){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(mensajeEstadoTiempo);
-  lcd.setCursor(0,1);
-  lcd.print(mensajeUsuario);
 }
 
 void humedadTemperatura() {
@@ -145,4 +110,374 @@ void luminosidad() {
   lcd.print(valorLuminosidad); // Imprimimos en pantalla el valor actualizado de luminosidad actual
   lcd.write(" Lux");
   //delay(1000); // Esperamos 1 segundo entre datos mostrados de luminosidad en pantalla LCD
+}
+
+void fechaHora(){
+  time_t t = now();//Declaramos la variable time_t t
+
+//Imprimimos la fecha y lahora
+lcd.setCursor(0,0);
+  if(day(t)>=0 && day(t)<=9)
+  { lcd.write("0");
+    lcd.print(day(t)); }
+  else
+  {lcd.print(day(t));}  
+  lcd.write(+ "/") ;
+  
+  if(month(t)>=0 && month(t)<=9)
+  { lcd.write("0");
+  lcd.print(month(t));}
+  else
+  {lcd.print(month(t));}
+  lcd.write(+ "/") ;
+  
+  lcd.print(year(t)); 
+  lcd.write(" ") ;
+
+  if(hour(t)>=0 && hour(t)<=9)
+  { lcd.write("0");
+  lcd.print(hour(t)); }
+  else
+  {lcd.print(hour(t));}
+  lcd.write(+ ":") ;
+
+  if(minute(t)>=0 && minute(t)<=9)
+  { lcd.write("0");
+  lcd.print(minute(t));}
+  else
+  {lcd.print(minute(t));}
+  lcd.write(":") ;
+  
+  if(second(t)>=0 && second(t)<=9)
+  { lcd.write("0");
+    lcd.print(second(t));}
+  else
+  {lcd.print(second(t));}
+  delay(500);//Esperamos 1 segundo
+}
+
+void LCD(){
+  int cuenta=0;
+int caracteres=0;
+while (Serial.available()>0){
+Mensaje=Mensaje+Decimal_to_ASCII(Serial.read());
+cuenta++;
+}
+caracteres=Mensaje.length();
+//if (caracteres>20){
+  if (Mensaje!=""){
+ lcd.clear();
+ //lcd.setCursor(0,0);
+ lcd.print(Mensaje.substring(0,20));
+ lcd.setCursor(0,1); 
+ lcd.print(Mensaje.substring(20,40));
+ lcd.setCursor(0,2); 
+ lcd.print(Mensaje.substring(40,caracteres));
+  delay(5000);   
+    
+}
+//}
+//else
+//{
+//if (Mensaje!=""){
+// lcd.clear();
+// lcd.setCursor(0,1);
+// lcd.print(Mensaje);
+// delay(5000);
+//}
+//}
+
+Mensaje="";
+}
+
+char Decimal_to_ASCII(int entrada){
+  char salida=' ';
+  switch(entrada){
+case 32: 
+salida=' '; 
+break; 
+case 33: 
+salida='!'; 
+break; 
+case 34: 
+salida='"'; 
+break; 
+case 35: 
+salida='#'; 
+break; 
+case 36: 
+salida='$'; 
+break; 
+case 37: 
+salida='%'; 
+break; 
+case 38: 
+salida='&'; 
+break; 
+case 39: 
+salida=' '; 
+break; 
+case 40: 
+salida='('; 
+break; 
+case 41: 
+salida=')'; 
+break; 
+case 42: 
+salida='*'; 
+break; 
+case 43: 
+salida='+'; 
+break; 
+case 44: 
+salida=','; 
+break; 
+case 45: 
+salida='-'; 
+break; 
+case 46: 
+salida='.'; 
+break; 
+case 47: 
+salida='/'; 
+break; 
+case 48: 
+salida='0'; 
+break; 
+case 49: 
+salida='1'; 
+break; 
+case 50: 
+salida='2'; 
+break; 
+case 51: 
+salida='3'; 
+break; 
+case 52: 
+salida='4'; 
+break; 
+case 53: 
+salida='5'; 
+break; 
+case 54: 
+salida='6'; 
+break; 
+case 55: 
+salida='7'; 
+break; 
+case 56: 
+salida='8'; 
+break; 
+case 57: 
+salida='9'; 
+break; 
+case 58: 
+salida=':'; 
+break; 
+case 59: 
+salida=';'; 
+break; 
+case 60: 
+salida='<'; 
+break; 
+case 61: 
+salida='='; 
+break; 
+case 62: 
+salida='>'; 
+break; 
+case 63: 
+salida='?'; 
+break; 
+case 64: 
+salida='@'; 
+break; 
+case 65: 
+salida='A'; 
+break; 
+case 66: 
+salida='B'; 
+break; 
+case 67: 
+salida='C'; 
+break; 
+case 68: 
+salida='D'; 
+break; 
+case 69: 
+salida='E'; 
+break; 
+case 70: 
+salida='F'; 
+break; 
+case 71: 
+salida='G'; 
+break; 
+case 72: 
+salida='H'; 
+break; 
+case 73: 
+salida='I'; 
+break; 
+case 74: 
+salida='J'; 
+break; 
+case 75: 
+salida='K'; 
+break; 
+case 76: 
+salida='L'; 
+break; 
+case 77: 
+salida='M'; 
+break; 
+case 78: 
+salida='N'; 
+break; 
+case 79: 
+salida='O'; 
+break; 
+case 80: 
+salida='P'; 
+break; 
+case 81: 
+salida='Q'; 
+break; 
+case 82: 
+salida='R'; 
+break; 
+case 83: 
+salida='S'; 
+break; 
+case 84: 
+salida='T'; 
+break; 
+case 85: 
+salida='U'; 
+break; 
+case 86: 
+salida='V'; 
+break; 
+case 87: 
+salida='W'; 
+break; 
+case 88: 
+salida='X'; 
+break; 
+case 89: 
+salida='Y'; 
+break; 
+case 90: 
+salida='Z'; 
+break; 
+case 91: 
+salida='['; 
+break; 
+case 92: 
+salida=' '; 
+break; 
+case 93: 
+salida=']'; 
+break; 
+case 94: 
+salida='^'; 
+break; 
+case 95: 
+salida='_'; 
+break; 
+case 96: 
+salida='`'; 
+break; 
+case 97: 
+salida='a'; 
+break; 
+case 98: 
+salida='b'; 
+break; 
+case 99: 
+salida='c'; 
+break; 
+case 100: 
+salida='d'; 
+break; 
+case 101: 
+salida='e'; 
+break; 
+case 102: 
+salida='f'; 
+break; 
+case 103: 
+salida='g'; 
+break; 
+case 104: 
+salida='h'; 
+break; 
+case 105: 
+salida='i'; 
+break; 
+case 106: 
+salida='j'; 
+break; 
+case 107: 
+salida='k'; 
+break; 
+case 108: 
+salida='l'; 
+break; 
+case 109: 
+salida='m'; 
+break; 
+case 110: 
+salida='n'; 
+break; 
+case 111: 
+salida='o'; 
+break; 
+case 112: 
+salida='p'; 
+break; 
+case 113: 
+salida='q'; 
+break; 
+case 114: 
+salida='r'; 
+break; 
+case 115: 
+salida='s'; 
+break; 
+case 116: 
+salida='t'; 
+break; 
+case 117: 
+salida='u'; 
+break; 
+case 118: 
+salida='v'; 
+break; 
+case 119: 
+salida='w'; 
+break; 
+case 120: 
+salida='x'; 
+break; 
+case 121: 
+salida='y'; 
+break; 
+case 122: 
+salida='z'; 
+break; 
+case 123: 
+salida='{'; 
+break; 
+case 124: 
+salida='|'; 
+break; 
+case 125: 
+salida='}'; 
+break; 
+case 126: 
+salida='~'; 
+break; 
+  }
+  return salida;
 }
